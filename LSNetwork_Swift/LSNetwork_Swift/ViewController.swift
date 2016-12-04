fb0d07e43cc537e7994fa0df49ceb129d2927327
lsrain <315192042@qq.com>
2016-12-04 18:10:10 +0800

    封装`extension`外部单独接口

--- LSNetwork_Swift/LSNetwork_Swift/ViewController.swift
+++ LSNetwork_Swift/LSNetwork_Swift/ViewController.swift
@@ -12,14 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        LSNetworkTools.sharedTools.loadWeather(cityName: "北京", success: { (responseObject) in
+            print("\(responseObject)")
+        }) { (error) in
+            print("\(error)")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
